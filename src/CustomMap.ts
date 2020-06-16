9aafdc6370265d8f721b8e67999bcb4eeef6aab1
Gauthier Riou <gauthier.riou@airbus.com>
2020-06-16 13:45:59 +0200

    feat: Can add a marker for a random company

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -18,5 +18,10 @@ export class CustomMap {
       position: { lat: user.location.lat, lng: user.location.lng }
     })
   }
-  addCompanyMarker(company: Company): void {}
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: { lat: company.location.lat, lng: company.location.lng }
+    })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { CustomMap } from './CustomMap'
 import { User } from './User'
+import { Company } from './Company'
 
 const user = new User()
-console.log(user.location)
+const company = new Company()
 const customMap = new CustomMap('map')
+
 customMap.addUserMarker(user)
+customMap.addCompanyMarker(company)
